12e17f78327acf8ea5c71dad6051210540f4ca18
Paul <paul.kiplagt@student.moringaschool.com>
2022-08-19 16:03:17 +0300

    done

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,7 @@
 # require the environment file
 
+#!/usr/bin/env ruby
+
 require_relative '../config/environment'
 
 hash = { :a => 4, :b => 10 }
--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,2 @@
-# Set up Bundler here following the instructions in the README
 require 'bundler/setup'
 Bundler.require(:default, :development)
